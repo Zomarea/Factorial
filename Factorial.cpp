a16fa30259bfd1aa3cde06c2c28d3a273dd172e7
Zomarea <acasterkian@gmail.com>
2017-04-23 17:39:56 +0100

    Improvements

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,18 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int factorial(int x) {
-  if(x == 1) return x;
-  else
-  return x * factorial(x - 1);
+float factorial(float x) {
+      return(x == 1) ? x : x * factorial(x-1);
 }
 
 int main(){
-	int n;
-	int r;
+	float n;
 	cout << "Work out the factorial of..." << endl;
 	cin >> n;
-	r = factorial(n);
-	cout << "The factorial of " << n << " is " << r;
+	cout << "The factorial of " << n << " is " << factorial(n);;
 }
 
